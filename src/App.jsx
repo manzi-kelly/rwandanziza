bafed5ebd39d030e4e7a0cb11108a456af367a09
manzi-kelly <manzikelly07@gmail.com>
2026-05-10 21:17:55 +0200

    update error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -6,26 +7,57 @@ import Services from "./components/Services";
 import Gallery from "./components/Gallery";
 import Testimonials from "./components/Testimonials";
 import Blog from "./components/Blog";
-import Footer from "./components/Footer";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="relative overflow-hidden bg-white">
+
+      {/* GLOBAL HEADER */}
       <Header />
+
+      {/* MAIN CONTENT */}
       <main>
+
+        {/* HERO */}
         <section id="home">
           <Hero />
         </section>
-        <About />
-        <Services />
+
+        {/* ABOUT */}
+        <section id="about">
+          <About />
+        </section>
+
+        {/* SERVICES */}
+        <section id="services">
+          <Services />
+        </section>
+
+        {/* GALLERY */}
         <section id="gallery">
           <Gallery />
         </section>
-        <Contact />
-        <Testimonials />
-        <Blog />
+
+        {/* TESTIMONIALS */}
+        <section id="testimonials">
+          <Testimonials />
+        </section>
+
+              {/* CONTACT */}
+        <section id="contact">
+          <Contact />
+        </section>
+
+        {/* BLOG */}
+        <section id="blog">
+          <Blog />
+        </section>
+
       </main>
+
+      {/* FOOTER */}
       <Footer />
     </div>
   );
